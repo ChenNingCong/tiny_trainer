d047c0f45d5a955f8a11e94c87873415ffb49d25
Your Name <nchen3@wpi.edu>
2024-09-13 00:46:00 -0400

    Formatting and add suport for template generation.

--- base_config.py
+++ base_config.py
@@ -4,41 +4,48 @@ from dataclasses import dataclass, field
 from omegaconf import MISSING
 from hydra.core.config_store import ConfigStore
 
-@dataclass 
+
+@dataclass
 class ProfilerConfig:
-    profile_memory : bool =True 
-    record_shapes : bool=True
-    with_flops : bool =True
-    with_stack : bool = False
+    profile_memory: bool = True
+    record_shapes: bool = True
+    with_flops: bool = True
+    with_stack: bool = False
     # scheduler setting
-    skip_first : int =5
-    wait : int =2
-    warmup : int =2
-    active : int =5
-    repeat : int =1
+    skip_first: int = 5
+    wait: int = 2
+    warmup: int = 2
+    active: int = 5
+    repeat: int = 1
+
 
 @dataclass
 class TrainerConfig:
     # ddp setting
-    world_size : int
+    world_size: int = MISSING
     # training setting
-    epoch : int
-    gradient_accum_step : int = 1
-    dtype : str = "torch.float32"
-    clip_norm : float = 1.0
+    epoch: int = MISSING
+    gradient_accum_step: int = 1
+    dtype: str = "torch.float32"
+    clip_norm: float = 1.0
     # evaluation setting
-    eval_n_sample : int = MISSING
-    save_n_sample : int = MISSING
+    eval_n_sample: int = MISSING
+    save_n_sample: int = MISSING
     # torch.compile setting
-    compile_model : bool = False
+    compile_model: bool = False
     # wandb setting
-    enable_wandb : bool = False
-    project_name : str = MISSING
-    run_name : str = MISSING
+    enable_wandb: bool = False
+    project_name: str = MISSING
+    run_name: str = MISSING
     # profiler setting
-    enable_profile : bool = False
-    profiler_config : ProfilerConfig = field(default_factory=ProfilerConfig)
+    enable_profile: bool = False
+    profiler_config: ProfilerConfig = field(default_factory=ProfilerConfig)
+
 
 # register name group
 cs = ConfigStore.instance()
 cs.store(group="base", name="base_trainer", node=TrainerConfig)
+
+def generate_template():
+    cfg = TrainerConfig()
+    print(OmegaConf.to_yaml(cfg))
